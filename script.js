eb94263e6fd5c06087e5406f48a0a3386bd8a4c7
mimi1389 <78655362+mimi1389@users.noreply.github.com>
2021-09-28 00:22:49 -0700

    init

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-
 $(document).ready(function(){
-  $("a").on('click', function(event) {
-
-    if (this.hash !== "") {
-     
-      event.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
-  });
-});
-
-$.fn.ready(function(){
-$("h4").fitText(1.2, { minFontSize: '14', maxFontSize: '34' });
-
+ 
+ $("#btnscrolltotop").click(function(){
+  $('html, body').animate({scrollTop : 0},10000);
+  
+ });
 });
